53590c81922ad7813e9940b7f45b91d39da91527
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-02-19 18:13:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-// complete this js code
-function Person(name, age) {}
+// // complete this js code
+// function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
+// function Employee(name, age, jobTitle) {}
+
+
+class Person {
+    constructor(name,age){
+        this.name = name;
+        this.age = age
+    }
+    greet(){
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+    }
+  }
+
+
+class Employee extends Person{
+    constructor(name,age,jobTitle){
+        super(name,age);
+        this.jobTitle = jobTitle
+    }
+    jobGreet(){
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+   
+    }
+}
+
+let emp = new Employee("Bob",30,"Manager")
+console.log(emp.jobGreet())
 
 // Do not change code below this line
 window.Person = Person;
